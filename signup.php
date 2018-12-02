24f36b914f95e5cc74fd4eff98731300679cd7a9
Aliazhari <Aliazhari@hotmail.com>
2018-12-02 18:43:03 -0500

    more fix of signup

--- signup.php
+++ signup.php
@@ -16,7 +16,7 @@ include "header.php";
 		<div class="col-md-4 col-md-offset-4">
     		<div class="panel panel-default">
 			  	<div class="panel-heading">
-			    	<h3 class="panel-title">Please fill in the form bellow</h3>
+			    	<h3 class="panel-title">Please fill in the form below</h3>
 			 	</div>
 			  	<div class="panel-body">
 			    	<form action="index.php" method="post">
