8adbe7b60f6182e63870175e006e241e7254d672
Aliazhari <Aliazhari@hotmail.com>
2018-12-02 22:21:01 -0500

    Add bio to about.php

--- about.php
+++ about.php
@@ -12,18 +12,14 @@ include "header.php";
 	</div>
 	<div class="text-center">
 		<a href="index.html"><img class="text-center triangle"
-			src="images/triangle_image.png" alt=""></a> <br>
-		<br>
-		<br>
+			src="images/triangle_image.png" alt=""></a> <br> <br> <br>
 		<div class="home_img">
 			<img src="images/white_bar.png" alt="">
 		</div>
 	</div>
 
 
-	<br>
-	<br>
-	<br>
+	<br> <br> <br>
 
 
 	<div id="new_album" class="container-fluid">
@@ -54,13 +50,11 @@ include "header.php";
 
 			</div>
 		</div>
-		<br>
-		<br>
+		<br> <br>
 		<div class="home_img text_center">
 			<img src="images/white_bar.png" alt="">
 		</div>
-		<br>
-		<br>
+		<br> <br>
 	</div>
 
 
@@ -76,146 +70,143 @@ include "header.php";
 				<div class="thumbnail">
 					<a href="images/ryan.png"> <img src="images/ryan.png" alt="Band"
 						style="width: 100%">
-						</div>
-					</a>
+				
 				</div>
+				</a>
+			</div>
 
 			<div class="col-sm-7">
 				<span class="col-left">
 					<h3>Ryan</h3> <br>
-					<p>Zero Hour consists of five young men who work varying jobs
-						across different professions from an auto mechanic to a sales
-						representative. They found their friendship in the hardcore scene
-						and decided to create the same content that brought them together.
-						When they are not working their full time jobs, they collaborate
-						on creating new music and would like a website that will allow
-						them to exhibit their creative work while also allowing them more
-						fan engagement.</p>
+					<p>Born and raised in central Connecticut, Ryan was introduced to
+						hardcore music by his older brother and has fell in love with it
+						ever sense. The high of being on stage, playing for an intense
+						crowd, and getting lost in the music is better than any drug,
+						which is why Ryan maintains a straightedge lifestyle. Ryan
+						graduated from the University of Hartford and moved to
+						Springfield, Massachusetts to pursue a career in sales..</p>
 				</span>
 			</div>
 		</div>
-		
+
 		<hr>
-		
+
 		<div class="row">
-			
+
 			<div class="col-sm-5">
 				<div class="thumbnail">
-					<a href="images/gonzalo.png"> <img src="images/gonzalo.png" alt="Band"
-						style="width: 100%">
-						</div>
-					</a>
+					<a href="images/gonzalo.png"> <img src="images/gonzalo.png"
+						alt="Band" style="width: 100%">
+				
 				</div>
+				</a>
+			</div>
 
 			<div class="col-sm-7">
 				<span class="col-left">
 					<h3>Gonzalo</h3> <br>
-					<p>Zero Hour consists of five young men who work varying jobs
-						across different professions from an auto mechanic to a sales
-						representative. They found their friendship in the hardcore scene
-						and decided to create the same content that brought them together.
-						When they are not working their full time jobs, they collaborate
-						on creating new music and would like a website that will allow
-						them to exhibit their creative work while also allowing them more
-						fan engagement.</p>
+					<p>Ex-navy. Gonzalo hails from a small town in Massachusetts where
+						his love for hardcore music begin. He visited local shows since he
+						was a young teenager and actively participated in the scene,
+						eventually become involved in the Boston and Connecticut scenes.
+						Since his return from traveling across the world on a Naval ship,
+						Gonzo begin writing his own music and singing lead vocals. Zero
+						Hour is his main focus, but he has other side projects in other
+						genres he is passionate about.</p>
 				</span>
 			</div>
 		</div>
 		<hr>
 		<div class="row">
-			
+
 			<div class="col-sm-5">
 				<div class="thumbnail">
 					<a href="images/rob.png"> <img src="images/rob.png" alt="Band"
 						style="width: 100%">
-						</div>
-					</a>
+				
 				</div>
+				</a>
+			</div>
 
 			<div class="col-sm-7">
 				<span class="col-left">
 					<h3>Rob</h3> <br>
-					<p>Zero Hour consists of five young men who work varying jobs
-						across different professions from an auto mechanic to a sales
-						representative. They found their friendship in the hardcore scene
-						and decided to create the same content that brought them together.
-						When they are not working their full time jobs, they collaborate
-						on creating new music and would like a website that will allow
-						them to exhibit their creative work while also allowing them more
-						fan engagement.</p>
+					<p>Originally from Florida, Rob moved to western Massachusetts with
+						his family when he was ten years old. He learned guitar shortly
+						after playing Guitar Hero with his friends and wanting to play
+						music for real. Rob went to school at Westfield state studying
+						marketing. He currently works for a marketing firm in Hartford and
+						assists in creating promotional materials for the band.</p>
 				</span>
 			</div>
 		</div>
-		
+
 		<hr>
-		
+
 		<div class="row">
-			
+
 			<div class="col-sm-5">
 				<div class="thumbnail">
 					<a href="images/sean.png"> <img src="images/sean.png" alt="Band"
 						style="width: 100%">
-						</div>
-					</a>
+				
 				</div>
+				</a>
+			</div>
 
 			<div class="col-sm-7">
 				<span class="col-left">
 					<h3>Sean</h3> <br>
-					<p>Zero Hour consists of five young men who work varying jobs
-						across different professions from an auto mechanic to a sales
-						representative. They found their friendship in the hardcore scene
-						and decided to create the same content that brought them together.
-						When they are not working their full time jobs, they collaborate
-						on creating new music and would like a website that will allow
-						them to exhibit their creative work while also allowing them more
-						fan engagement.</p>
+					<p>Sean is a full time musician from western Massachusetts. He
+						began learning the trumpet when he was younger and picked up
+						guitar in his teenage years. He currently teaches music at a
+						middle school and teaches privately on the side. His has many
+						influences across genres, and constantly tries to develop new
+						music on his own. He enjoys creating hardcore music the most, as
+						he loves the intensity of crowd while playing live.</p>
 				</span>
 			</div>
 		</div>
-		
+
 		<hr>
-		
+
 		<div class="row">
 
 			<div class="col-sm-5">
 				<div class="thumbnail">
-					<a href="images/justin.png"> <img src="images/justin.png" alt="Band"
-						style="width: 80%">
-						</div>
-					</a>
+					<a href="images/justin.png"> <img src="images/justin.png"
+						alt="Band" style="width: 80%">
+				
 				</div>
+				</a>
+			</div>
 
 			<div class="col-sm-7">
 				<span class="col-left">
 					<h3>Justin</h3> <br>
-					<p>Zero Hour consists of five young men who work varying jobs
-						across different professions from an auto mechanic to a sales
-						representative. They found their friendship in the hardcore scene
-						and decided to create the same content that brought them together.
-						When they are not working their full time jobs, they collaborate
-						on creating new music and would like a website that will allow
-						them to exhibit their creative work while also allowing them more
-						fan engagement.</p>
+					<p>Also a native from Connecticut, Justin met Ryan while at college
+						and began going to shows with him on the weekends. He learned Bass
+						when he was a young kid from his father and began using his
+						talents in various bands since he was a teenager. Justin works as
+						a mechanic in Hartford as a profession. Recently married to his
+						long-time girlfriend, he is excited to start a family soon.</p>
 				</span>
 			</div>
 		</div>
-		
+
 
 	</div>
-	<br>
-	<br>
+	<br> <br>
 	<div class="home_img text_center">
 		<img src="images/white_bar.png" alt="">
 	</div>
-	<br>
-	<br>
+	<br> <br>
 </div>
 
 
 <br>
 <br>
 
-<?php 
-    include "footer.php";
+<?php
+include "footer.php";
 ?>
